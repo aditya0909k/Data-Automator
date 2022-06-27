77fe77a13f2cfdd9dcc7e7eabad52609528743bc
aditya0909k <85547196+aditya0909k@users.noreply.github.com>
2022-06-27 17:42:46 -0500

    cleaned up commented lines

--- dist/excel.py
+++ dist/excel.py
@@ -5,23 +5,13 @@ path = os.path.dirname(sys.executable)
 
 inFile = input('Enter input file: ')
 outFile = input('Enter output file: ')
-#file = './sample.csv'
+
 df = pd.read_csv(f'{path}/{inFile}')
 
-# with open(inFile,'r') as i:
-#     lines = i.readlines()
-
-#def manipulateData(lines):
 for col in df.columns:
     df[col] = df[col].replace("\\N", "")
-    #df[col] = df[col].str.replace(",", "")
     df[col] = df[col].replace(',', '', regex=True)
     df[col] = df[col].replace('"', '', regex=True)
-
-#processedData = manipulateData(lines)
-# with open(outFile,'w') as o:
-#     for col in df.columns:
-#         o.write(col)
 
 #print(df)
 
